f0155dad8e663d7e36d3758180128e046a9772f7
w0rng <antonabramov2000@gmail.com>
2021-01-18 18:16:43 +0700

    Модель пользователя

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    full_name = models.CharField('ФИО', max_length=255)
+    address = models.CharField('Адрес проживания', max_length=255)
+    login = models.CharField('Логин', max_length=255, unique=True)
+    email = models.EmailField('Почта', unique=True)
+    date_joined = models.DateTimeField('Дата регистрации', auto_now_add=True)
+    password = models.CharField('Хэш пароля', max_length=128)
+    token = models.CharField('Токен', max_length=10)
