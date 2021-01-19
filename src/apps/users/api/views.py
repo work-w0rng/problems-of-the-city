39140173ff814c07d3abc01668830333f400ca5d
w0rng <antonabramov2000@gmail.com>
2021-01-19 10:27:12 +0700

    метод для авторизации пользователя

--- src/apps/users/api/views.py
+++ src/apps/users/api/views.py
@@ -1,3 +1,4 @@
+from django.http import response
 from . import router, schemas
 from .. import models
 from django.core.exceptions import ValidationError
@@ -12,8 +13,7 @@ from typing import List
         402: List[schemas.Error]
     }
 )
-def create(request, user: schemas.Registration):
-
+def register(request, user: schemas.Registration):
     if models.User.objects.filter(email=user.email):
         return 401, {'message': 'Данный email уже занят'}
 
@@ -30,3 +30,20 @@ def create(request, user: schemas.Registration):
         return 402, [{'message': message} for message in error.messages]
 
     return 200, {'token': user.token}
+
+
+@router.post(
+    '/login/',
+    response={
+        200: schemas.Token,
+        401: schemas.Error
+    }
+)
+def login(request, user: schemas.Login):
+    user_ = models.User.objects.filter(email=user.email).first()
+    if not user_:
+        return 401, {'message': 'Введите правильную почту или пароль'}
+    if not user_.check_password(user.password):
+        return 401, {'message': 'Введите правильную почту или пароль'}
+    
+    return 200, {'token': user_.token}
