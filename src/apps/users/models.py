9fbe93203445674cbb627bac0109e460188c7a47
w0rng <antonabramov2000@gmail.com>
2021-01-19 08:59:21 +0700

    fix место жительства не обязательно заполнять

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -8,7 +8,7 @@ from string import ascii_letters, digits
 
 class User(models.Model):
     full_name = models.CharField('ФИО', max_length=255)
-    address = models.CharField('Адрес проживания', max_length=255)
+    address = models.CharField('Адрес проживания', max_length=255, blank=True, null=True)
     login = models.CharField('Логин', max_length=255, unique=True)
     email = models.EmailField('Почта', unique=True)
     date_joined = models.DateTimeField('Дата регистрации', auto_now_add=True)
