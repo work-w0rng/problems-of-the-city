ed092d9ff444d51cc61f0ddb82e137d7a9d484e1
w0rng <antonabramov2000@gmail.com>
2021-01-19 10:00:26 +0700

    Регистрация пользователя

--- src/apps/users/api/views.py
+++ src/apps/users/api/views.py
@@ -1,6 +1,18 @@
-from . import router
+from . import router, schemas
+from .. import models
 
 
-@router.post('/register/')
-def create(request):
-    pass
+@router.post(
+    '/register/', 
+    response={
+        200: schemas.Token,
+    }
+)
+def create(request, user: schemas.User):
+    user = models.User.objects.create(
+        full_name=user.full_name,
+        address=user.address,
+        email=user.email,
+        password=user.password,
+    )
+    return 200, {'token': user.token}
