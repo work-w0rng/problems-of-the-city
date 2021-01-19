bdc44dc1aa7f6d7571f686f8c4a92d056c234327
w0rng <antonabramov2000@gmail.com>
2021-01-19 17:07:23 +0700

    fix почему-то не работает без дебага

--- src/config/settings/env.py
+++ src/config/settings/env.py
@@ -1,6 +1,6 @@
 from os import environ
 
 
-DEBUG = environ.get('DEBUG', False)
+DEBUG = environ.get('DEBUG', True)
 SECRET_KEY = environ.get('SECRET_KEY', 'secret_key')
 ADMIN_URL = environ.get('ADMIN_URL', 'admin/')
