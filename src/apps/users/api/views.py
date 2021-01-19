5a6149b1ccba7fb31788b8a026f239b623538067
w0rng <antonabramov2000@gmail.com>
2021-01-19 10:01:24 +0700

    Проверка занят ли email

--- src/apps/users/api/views.py
+++ src/apps/users/api/views.py
@@ -5,14 +5,20 @@ from .. import models
 @router.post(
     '/register/', 
     response={
-        200: schemas.Token,
+        200: schemas.Token, 
+        401: schemas.Error,
     }
 )
 def create(request, user: schemas.User):
-    user = models.User.objects.create(
+
+    if models.User.objects.filter(email=user.email):
+        return 401, {'message': 'Данный email уже занят'}
+
+    user = models.User(
         full_name=user.full_name,
         address=user.address,
         email=user.email,
         password=user.password,
     )
+
     return 200, {'token': user.token}
