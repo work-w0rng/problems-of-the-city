e44bf04054e979ceceb60c0d3681578d3e64ba99
w0rng <antonabramov2000@gmail.com>
2021-01-20 15:37:19 +0700

    Коды ошибок при валидации пароля

--- src/apps/users/services/validators.py
+++ src/apps/users/services/validators.py
@@ -1,10 +1,11 @@
 from django.utils.regex_helper import _lazy_re_compile
 from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import get_default_password_validators
 
 
 class FullNameValidator:
     message = 'Введите ФИО в корректном формате'
-    code = 'invalid'
+    code = 'invalid_full_name'
     fio_regex = _lazy_re_compile(
         r"[a-zA-ZА-Яа-яЁё\-]{2,}( [a-zA-ZА-Яа-яЁё]{2,}){1,2}"
     )
@@ -18,6 +19,17 @@ class FullNameValidator:
     def __call__(self, value):
         if not self.fio_regex.match(value):
             raise ValidationError(self.message, code=self.code)
+
+
+def validate_password(password):
+    errors = []
+    for validator in get_default_password_validators():
+        try:
+            validator.validate(password, None)
+        except ValidationError as error:
+            errors.append(error.code)
+    if errors:
+        raise ValidationError(errors)
 
 
 validate_full_name = FullNameValidator()
