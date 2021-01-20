cf4ed0b1b82f91349ea3551b8690f1b1aaafc7ee
w0rng <antonabramov2000@gmail.com>
2021-01-20 14:54:50 +0700

    убрал лишние валдаторы

--- src/config/settings/settings.py
+++ src/config/settings/settings.py
@@ -78,13 +78,7 @@ TEMPLATES = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
