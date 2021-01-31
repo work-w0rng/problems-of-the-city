69d98656cf81cc237d376aacb2f48ec76ba4999a
w0rng <antonabramov2000@gmail.com>
2021-01-31 11:09:07 +0700

    fix выводить запросы только для api

--- src/config/settings/middleware.py
+++ src/config/settings/middleware.py
@@ -1,6 +1,8 @@
 def simple_middleware(get_response):
     def middleware(request):
-        print(f'Запрос {request.path}:', getattr(request, '_body', request.body))
+        if 'api' != request.path.split('/')[1]:
+            return get_response(request)
+        print(f'{request.method} запрос {request.path}:', getattr(request, '_body', request.body))
         response = get_response(request)
         print('Ответ: ', response._container)
         return response
